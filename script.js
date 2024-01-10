adbd1b8c84bbab6c89558006e8e08ad12874d75b
JHoldsworth23 <jacobph101@icloud.com>
2024-01-10 17:29:48 +0000

    Add the restart button to clear the grid and change the function name to displayGame

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const gameplay = (function () {
+const displayGame = (function () {
 
     const divElements = document.querySelectorAll('.grid-items');
+    const restartBtn = document.querySelector('.restart');
 
     divElements.forEach((div) => {
         div.addEventListener('click', () => {
             div.textContent = 'X';
         });
+    });
+
+    restartBtn.addEventListener('click', () => {
+        for (let i = 0; i < divElements.length; i++) {
+            divElements[i].textContent = '';
+        }
     });
 
 })();
